7891e29f7fac1dea709755d42610ead4d5ce01cc
Matthias Kümmerer <matthias@matthias-k.org>
2014-09-23 11:34:24 +0200

    setup.py
    Signed-off-by: Matthias Kümmerer <matthias@matthias-k.org>

---
+++ setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup
+
+setup(
+    name = "PyCircStat",
+    version = "0.0.1",
+    author = "Philipp Berens et al.",
+    author_email = "philipp.berens@uni-tuebingen.de",
+    description = ("Toolbox for circular statistics with Python"),
+    #license = "MIT",
+    keywords = "statistics",
+    #url = "http://packages.python.org/PyCircStat",
+    packages=['PyCircStat', 'tests'],
+    #long_description=read('README'),
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Topic :: Utilities",
+        #"License :: OSI Approved :: MIT License",
+    ],
+    setup_requires=['nose>=1.0'],
+)
