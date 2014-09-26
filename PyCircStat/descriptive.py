91299bddbee752a18d2eb808fa7c46eaf87ae2f4
Fabian Sinz <fabee@epagoge.de>
2014-09-26 17:38:22 +0200

    added full documentation of circular mean and bugfix in circular mean

--- PyCircStat/descriptive.py
+++ PyCircStat/descriptive.py
@@ -7,13 +7,18 @@ import numpy as np
 def mean(data, w=None, axis=0):
     """
     Compute mean direction of circular data.
+
+    :param data: circular data
+    :param w: 	 weightings in case of binned angle data
+    :param axis: compute along this dimension, default is 0
+    :return: circular mean
     """
 
     if w is None:
         w = np.ones_like(data)
 
     if data.shape != w.shape:
-        raise ValueError('Input dimensions do not match:', data.shape, alpha.shape)
+        raise ValueError('Input dimensions do not match:', data.shape)
 
     r = (w * np.exp(1j*data)).sum(axis=axis)
 
