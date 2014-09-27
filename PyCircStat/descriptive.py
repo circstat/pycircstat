bugfix in mean computation (one-dimensional CI)

PyCircStat/descriptive.py
@@ -7,7 +7,7 @@ from scipy import stats
 import warnings
 
 
-def mean(alpha, w=None, ci = None, d=None, axis=0):
+def mean(alpha, w=None, ci=None, d=None, axis=0):
     """
     Compute mean direction of circular data.
 
@@ -40,9 +40,7 @@ def mean(alpha, w=None, ci = None, d=None, axis=0):
         return mu
     else:
         t = mean_ci_limits(alpha, ci=ci, w=w, d=d, axis=axis)
-        return mu, (mu + t, mu - t)
-
-
+        return mu, mu + t, mu - t
 
 
 def resultant_vector_length(alpha, w=None, d=None, axis=0):
@@ -100,8 +98,9 @@ def mean_ci_limits(alpha, ci=0.95, w=None, d=None, axis=0):
 
     assert alpha.shape == w.shape, "Dimensions of data and w do not match!"
 
-    r = resultant_vector_length(alpha, w=w, d=d, axis=axis)
-    n = np.sum(w, axis=axis)
+    r = np.atleast_1d(resultant_vector_length(alpha, w=w, d=d, axis=axis))
+    n = np.atleast_1d(np.sum(w, axis=axis))
+
     R = n * r
     c2 = stats.chi2.ppf(ci, df=1)
 

tests/test_descriptive.py
@@ -21,6 +21,7 @@ def test_resultant_vector_length_axis():
     data = np.ones((10,2))
     assert_allclose(PyCircStat.resultant_vector_length(data, axis=1), np.ones(10))
 
+
 def test_mean_ci_limits():
     data = np.array([
             [0.58429,   0.88333],
@@ -34,7 +35,7 @@ def test_mean_ci_limits():
     assert_allclose(PyCircStat.mean_ci_limits(data, ci=0.8, axis=0), out1, rtol=1e-4)
     assert_allclose(PyCircStat.mean_ci_limits(data, ci=0.8, axis=1), out2, rtol=1e-4)
 
-def test_mean_ci():
+def test_mean_ci_2d():
     data = np.array([
             [0.58429,   0.88333],
             [1.14892 ,  2.22854],
@@ -46,10 +47,20 @@ def test_mean_ci():
     muminus = np.array([np.NaN, 0.89931])
     mu = np.array([1.6537, 1.7998])
 
-    mu_tmp, (muplus_tmp, muminus_tmp) = PyCircStat.mean(data, ci=0.95, axis=0)
+    mu_tmp, muplus_tmp, muminus_tmp = PyCircStat.mean(data, ci=0.95, axis=0)
     assert_allclose(muplus, muplus_tmp, rtol=1e-4)
     assert_allclose(muminus, muminus_tmp, rtol=1e-4)
     assert_allclose(mu, mu_tmp, rtol=1e-4)
 
+def test_mean_ci_1d():
+    data = np.array([0.88333, 2.22854, 3.06369, 1.49836, 1.51748])
+    muplus = 2.7003
+    muminus = 0.89931
+    mu = 1.7998
+
+    mu_tmp, muplus_tmp, muminus_tmp = PyCircStat.mean(data, ci=0.95)
+    assert_allclose(muplus, muplus_tmp, rtol=1e-4)
+    assert_allclose(muminus, muminus_tmp, rtol=1e-4)
+    assert_allclose(mu, mu_tmp, rtol=1e-4)
 
 