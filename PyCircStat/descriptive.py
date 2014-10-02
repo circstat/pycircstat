c3aa383f5cabae482d18118c33df3665b7dba37c
Fabian Sinz <fabian.sinz@uni-tuebingen.de>
2014-10-02 14:27:30 +0200

    Switched to namedtuple as confidence interval and return non-bootstrapped value for bootstrapped CIs

--- PyCircStat/descriptive.py
+++ PyCircStat/descriptive.py
@@ -60,12 +60,8 @@ def median(alpha, axis=0, ci=None, bootstrap_max_iter=1000):
         r = [median(a, ci=None, axis=axis) for a in nd_bootstrap((alpha,) , min(alpha.shape[axis], bootstrap_max_iter), axis=axis)]
 
         ci_low, ci_high = np.percentile(r, [(1 - ci) / 2 * 100, (1 + ci) / 2 * 100], axis=0) # TODO: write circular percentile (opposite)
-<<<<<<< HEAD
         return median(alpha, ci=None, axis=axis), CI(ci_low, ci_high)
-=======
-        return mean(r, axis=0), ci_low, ci_high
-
->>>>>>> intermediate commit
+        return median(alpha, ci=None, axis=axis), CI(ci_low, ci_high)
 
 def cdiff(alpha, beta):
     """
@@ -252,14 +248,10 @@ def corrcc(alpha1, alpha2, ci=None, axis=0, bootstrap_max_iter=1000):
         return num / den
     else:
         r = [corrcc(a1, a2, ci=None, axis=axis) for a1, a2 in
-             nd_bootstrap((alpha1, alpha2), min(alpha1.shape[axis], bootstrap_max_iter), axis=axis)] # TODO: new bootstrap iteration number
+             nd_bootstrap((alpha1, alpha2), min(alpha1.shape[axis], bootstrap_max_iter), axis=axis)]
 
         ci_low, ci_high = np.percentile(r, [(1 - ci) / 2 * 100, (1 + ci) / 2 * 100], axis=0)
-<<<<<<< HEAD
         return corrcc(alpha1, alpha2, ci=None, axis=axis), CI(ci_low, ci_high)
-=======
-        return np.mean(r, axis=0), ci_low, ci_high #TODO return r itself? also in median
->>>>>>> intermediate commit
 
 @mod2pi
 def center(*args, **kwargs):
