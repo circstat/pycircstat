0999861b8e0595d2265be4fd57bff15cc520aa0b
Philipp Berens <philipp.berens@uni-tuebingen.de>
2014-10-29 09:48:31 +0100

    removed utils test

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -7,9 +7,6 @@ from numpy.testing import assert_allclose
 from pycircstat import utils
 
 
-def test_rad2ang_ang2rad():
-    assert_allclose(np.pi, utils.ang2rad(utils.rad2ang(np.pi)), atol=0.001, rtol=0.001)    
-    assert_allclose(180., utils.rad2ang(utils.ang2rad(180.)), atol=0.001, rtol=0.001)
 
 
 
