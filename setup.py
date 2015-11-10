d30f6bfa08b548d28906f05d61bbcd920815169d
Fabian Sinz <sinz@bcm.edu>
2015-11-10 11:32:46 -0600

    add scipy to requirements and change version to 0.0.2

--- setup.py
+++ setup.py
@@ -2,8 +2,8 @@ from setuptools import setup
 
 setup(
     name = "pycircstat",
-    version = "0.0.1",
-    author = "Philipp Berens et al.",
+    version = "0.0.2",
+    author = "Fabian Sinz, Philipp Berens, Matthias Kuemmerer, Tom Wallis",
     author_email = "philipp.berens@uni-tuebingen.de",
     description = ("Toolbox for circular statistics with Python"),
     #license = "MIT",
@@ -16,5 +16,5 @@ setup(
         "Topic :: Utilities",
         #"License :: OSI Approved :: MIT License",
     ],
-    setup_requires=['nose>=1.0', 'mock', 'sphinx_rtd_theme', "decorator", "pandas"],
+    setup_requires=['nose>=1.0', 'mock', 'sphinx_rtd_theme', "decorator", "pandas", "scipy"],
 )
